fd668591e35890927eab9ca5ee901f92beba78bb
NTran575 <nhantran575@gmail.com>
2021-07-04 00:44:19 -0400

    Update: new xml pages

--- app/src/main/java/com/dynamicdevs/mvvmgroupapi/view/activity/MainActivity.kt
+++ app/src/main/java/com/dynamicdevs/mvvmgroupapi/view/activity/MainActivity.kt
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.dynamicdevs.mvvmgroupapi.R
 import com.dynamicdevs.mvvmgroupapi.databinding.ActivityMainBinding
+import com.dynamicdevs.mvvmgroupapi.databinding.ActivitySearchidBinding
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
--- app/src/main/java/com/dynamicdevs/mvvmgroupapi/view/activity/SearchidActivity.kt
+++ app/src/main/java/com/dynamicdevs/mvvmgroupapi/view/activity/SearchidActivity.kt
@@ -3,10 +3,21 @@ package com.dynamicdevs.mvvmgroupapi.view.activity
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.dynamicdevs.mvvmgroupapi.R
+import com.dynamicdevs.mvvmgroupapi.databinding.ActivitySearchidBinding
 
 class SearchidActivity : AppCompatActivity() {
+    private lateinit var binding: ActivitySearchidBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_searchid)
+        binding = ActivitySearchidBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        //back to MainActivity
+        binding.backButton.setOnClickListener {
+
+        }
+        //towards image by url
+        binding.imageButton.setOnClickListener {
+
+        }
     }
 }
