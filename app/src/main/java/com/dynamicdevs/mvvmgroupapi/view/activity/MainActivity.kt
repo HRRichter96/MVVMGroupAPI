a465da001380d43c3a35b6bb6f56b27906d69295
NTran575 <nhantran575@gmail.com>
2021-07-04 00:03:26 -0400

    Update: new xml pages

--- app/src/main/java/com/dynamicdevs/mvvmgroupapi/view/activity/MainActivity.kt
+++ app/src/main/java/com/dynamicdevs/mvvmgroupapi/view/activity/MainActivity.kt
@@ -1,12 +1,22 @@
 package com.dynamicdevs.mvvmgroupapi.view.activity
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.dynamicdevs.mvvmgroupapi.R
+import com.dynamicdevs.mvvmgroupapi.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding= ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        //next page
+        binding.searchIdbutton.setOnClickListener {
+
+        }
     }
 }
